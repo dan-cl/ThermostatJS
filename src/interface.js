9f584649aaaa5a92e06b6ff372c3d0380d5b9add
Daniel Clissold <dan@Daniels-MacBook-Pro.local>
2017-04-02 16:05:21 +0100

    added weather api

--- src/interface.js
+++ src/interface.js
@@ -30,9 +30,28 @@ $(document).ready(function(){
     updateTemperature();
   });
 
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    displayWeather(city);
+  });
+
+
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temp').text(data.main.temp);
+  });
+
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.energyUsage());
+  }
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temp').text(data.main.temp);
+    });
   }
 
 });
